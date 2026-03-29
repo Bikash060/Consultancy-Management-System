8141667b10dd0f5d10ec69afb659cbcc63fead3c
Bikash Kumar Das <d13825653@gmail.com>
2026-03-29 10:00:00 +0545

    Update database schema with new tables and indexes

--- consultancy_db.sql
+++ consultancy_db.sql
@@ -7,6 +7,13 @@
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
+
+-- source venv/bin/activate
+
+-- python3
+--exit(0)
+--python3 run.py
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
